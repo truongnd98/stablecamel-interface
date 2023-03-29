9fa915ab005da2e47dd65906ca1c8f3e69c9a85b
luchitam1997 <chitam.lu97@gmail.com>
2023-03-29 16:55:57 +0700

    Update router, Tooltip chart, Metric

--- src/pages/yield/YieldChartTooltip.tsx
+++ src/pages/yield/YieldChartTooltip.tsx
@@ -1,69 +1,65 @@
 import { Box, Typography } from '@mui/material';
-
 import { convertCurrency } from '../../utils/convertCurrency';
 import { v4 } from 'uuid';
 
-const sumData = (list: any[]) => {
-  return list.reduce((partialSum, a) => partialSum + a.value, 0);
+const convertValue = (type: 'TVL' | 'APY', data: number) => {
+	switch (type) {
+		case 'TVL':
+			return new Intl.NumberFormat().format(data);
+		case 'APY':
+			return data.toFixed(2) + '%';
+	}
 };
 
 export function YieldChartTooltip({ active, payload, label }: any) {
-  // console.log('payload', payload);
-  if (active && payload && payload.length) {
-    return (
-      <Box
-        sx={{
-          width: 200,
-          display: 'flex',
-          flexDirection: 'column',
-          backgroundColor: '#ffffff',
-          padding: ' 12px',
-          borderRadius: '8px',
-          border: '2px solid #8c00ef',
-          '&.recharts-tooltip-wrapper': {
-            zIndex: 100,
-          },
-        }}
-      >
-        <Typography
-          variant='body1'
-          color='primary'
-        >
-          <b>{label}</b>
-        </Typography>
-        {payload.map((item: any) => (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-            key={v4()}
-          >
-            {item.value > 0 ? (
-              <>
-                <Typography
-                  variant='body1'
-                  sx={{
-                    color: item.color,
-                  }}
-                >{`${item.name}:`}</Typography>
-                <Typography
-                  variant='body1'
-                  sx={{
-                    color: item.color,
-                  }}
-                >
-                  {convertCurrency(item.value)}
-                </Typography>
-              </>
-            ) : (
-              <></>
-            )}
-          </Box>
-        ))}
-      </Box>
-    );
-  }
+	console.log('payload', payload, label, active);
+	if (active && payload && payload.length) {
+		return (
+			<Box
+				sx={{
+					width: 200,
+					display: 'flex',
+					flexDirection: 'column',
+					backgroundColor: '#ffffff',
+					padding: ' 12px',
+					borderRadius: '8px',
+					border: '2px solid #8c00ef',
+					'&.recharts-tooltip-wrapper': {
+						zIndex: 100
+					}
+				}}
+			>
+				<Typography
+					variant='body1'
+					color='primary'
+				>
+					<b>
+						{payload[0].payload.pool} on {payload[0].payload.project}
+					</b>
+				</Typography>
+				{payload.map((item: any, index: number) => (
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'space-between'
+						}}
+						key={v4()}
+					>
+						{item.value > 0 ? (
+							<>
+								<Typography variant='body1'>{`${item.name}:`}</Typography>
+								<Typography variant='body1'>
+									{convertValue(index === 0 ? 'TVL' : 'APY', item.value)}
+								</Typography>
+							</>
+						) : (
+							<></>
+						)}
+					</Box>
+				))}
+			</Box>
+		);
+	}
 
-  return null;
+	return null;
 }
