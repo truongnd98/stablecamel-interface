fe1730857ca2e2ae45f05923ddb420eb69495c75
Mat lu <chitam.lu97@gmail.com>
2023-04-07 18:39:03 +0700

    Adjust display of widget and content disclaimer

--- src/pages/trade-stablecoins/TradeStablecoins.tsx
+++ src/pages/trade-stablecoins/TradeStablecoins.tsx
@@ -3,22 +3,18 @@ import { TradeStablecoinsDisclaimer } from './components/TradeStablecoinsDisclai
 import { TradingWidget } from './components/TradingWidget';
 
 const main: SxProps = {
-	width: 'calc(100% - 240px)',
-	minHeight: '100vh',
-	height: '100%',
-	marginLeft: '240px',
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	flexDirection: 'column',
-	backgroundColor: '#f5f5f5'
+  width: 'calc(100% - 240px)',
+  minHeight: '100vh',
+  //   height: '100vh',
+  marginLeft: '240px',
+  backgroundColor: '#f5f5f5',
 };
 
 export default function TradeStablecoin() {
-	return (
-		<Box sx={main}>
-			<TradingWidget />
-			<TradeStablecoinsDisclaimer />
-		</Box>
-	);
+  return (
+    <Box sx={main}>
+      <TradingWidget />
+      <TradeStablecoinsDisclaimer />
+    </Box>
+  );
 }
--- src/pages/trade-stablecoins/components/TradingWidget.tsx
+++ src/pages/trade-stablecoins/components/TradingWidget.tsx
@@ -1,22 +1,14 @@
 import { Box } from '@mui/material';
 
 export function TradingWidget() {
-	return (
-		<Box
-			sx={{
-				width: '100%',
-				minHeight: '100vh',
-				height: '100%'
-			}}
-		>
-			<iframe
-				src='https://ramp.stably.io/?address=hx777535db1b4039c837580e74aac35d0bbaaa7b4c&amount=42.42&network=icon&asset=USDS&filter=true'
-				title='Stably Widget'
-				style={{
-					width: '100%',
-					height: '100%'
-				}}
-			/>
-		</Box>
-	);
+  return (
+    <iframe
+      src='https://ramp.stably.io/?address=hx777535db1b4039c837580e74aac35d0bbaaa7b4c&amount=42.42&network=icon&asset=USDS&filter=true'
+      title='Stably Widget'
+      style={{
+        width: '100%',
+        height: '100vh',
+      }}
+    />
+  );
 }
