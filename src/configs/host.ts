635259935b4987175ed239c2e35f462048166a80
truongdinh98 <ndtruonguit@gmail.com>
2023-07-14 10:44:49 +0700

    update host url

--- src/configs/host.ts
+++ src/configs/host.ts
@@ -1,2 +1,2 @@
 // export const HOST: string = process.env.REACT_APP_HOST_API || 'localhost';
-export const HOST: string = 'https://d3m79iu5hbobsp.cloudfront.net';
+export const HOST: string = 'https://d3vinh071ats9t.cloudfront.net';
